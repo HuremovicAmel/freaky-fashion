07668a19d8620b89c0fdde641f758b0f91371273
Amel Huremovic <amelhuremovic18@hotmail.com>
2026-06-03 17:33:27 +0200

    Added homepage and product cards

--- app.js
+++ app.js
@@ -4,9 +4,10 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var session = require('express-session');
+var db = require('./database/db');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// var indexRouter = require('./routes/index');
+// var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -25,8 +26,31 @@ app.use(session({
 }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
+
+app.get('/', function (req, res) {
+  db.all(`SELECT * FROM categories`, [], function (err, categories) {
+    if (err) return res.send('Database error');
+
+    db.all(`
+      SELECT *
+      FROM products
+      WHERE published_at <= DATE('now')
+      LIMIT 8
+    `, [], function (err, products) {
+      if (err) return res.send('Database error');
+
+      res.render('index', {
+        products: products,
+        categories: categories,
+        favoriteProductIds: req.session.favorites || [],
+        favoriteCount: (req.session.favorites || []).length,
+        cartCount: (req.session.cart || []).length
+      });
+    });
+  });
+});
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
